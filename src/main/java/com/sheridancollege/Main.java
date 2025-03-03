873f0d853a3735b159740a4122d312b8ba8d5427
Aanchal Baral <baralaa@sheridancollege.ca>
2025-03-03 12:19:11 -0500

    Renamed main.java to Main.java to fix class and file name mismatch

---
+++ src/main/java/com/sheridancollege/Main.java
@@ -0,0 +1,7 @@
+package com.sheridancollege;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, I am Aanchal Baral!");
+    }
+}
--- src/main/java/com/sheridancollege/main.java
+++
@@ -1,8 +0,0 @@
-package com.sheridancollege;
-public class main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hi, I am Aanchal Baral!" );
-    }
-}
